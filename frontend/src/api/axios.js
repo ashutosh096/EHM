af6d57e6b37ffb2703dbee1f6386249026dcc83d
exilonium <exilonium@proton.me>
2026-06-01 12:31:33 +0530

    fixing the local dev using an environment variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? "http://localhost:5000" : "https://ehm.onrender.com"),
-  baseURL: "/api/",
+  baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 // Attach token(if availabe) from localStorage to every request
