acffbb40214bd47a9a5b3bed275d972a8139575b
exilonium <exilonium@proton.me>
2026-06-01 12:51:38 +0530

    finally fixing the local and prod environment for vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: import.meta.env.DEV ? "http://localhost:5000" : "/api",
+  // baseURL: "/api/",
 });
 
 // Attach token(if availabe) from localStorage to every request
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
